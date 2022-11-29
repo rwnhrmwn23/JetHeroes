62974fa057f51860991f78c635f669eae1aa23fe
Irwan Hermawan <rwnhermawan@gmail.com>
2022-11-29 18:04:01 +0700

    Initial commit

---
+++ app/src/main/java/com/onedev/jetheroes/JetHeroesApp.kt
@@ -0,0 +1,143 @@
+package com.onedev.jetheroes
+
+import androidx.compose.animation.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.onedev.jetheroes.model.HeroesData
+import com.onedev.jetheroes.ui.theme.JetHeroesTheme
+import kotlinx.coroutines.launch
+
+@Composable
+fun JetHeroesApp(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier) {
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+        val showButton by remember {
+            derivedStateOf { listState.firstVisibleItemIndex > 0 }
+        }
+
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 80.dp)
+        ) {
+            items(HeroesData.heroes, key = { it.id }) {
+                HeroListItem(
+                    name = it.name,
+                    photoUrl = it.photoUrl,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+
+        AnimatedVisibility(
+            visible = showButton,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically(),
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            ScrollToTopButton(
+                onClick = {
+                    scope.launch {
+                        listState.scrollToItem(0)
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun HeroListItem(
+    name: String,
+    photoUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.clickable {
+
+        }
+    ) {
+        AsyncImage(
+            model = photoUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = name,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1F)
+                .padding(start = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun ScrollToTopButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .shadow(elevation = 10.dp, shape = CircleShape)
+            .clip(CircleShape)
+            .size(56.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.White,
+            contentColor = MaterialTheme.colors.primary
+        )
+    ) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun JetHeroesAppPreview() {
+    JetHeroesTheme {
+        JetHeroesApp()
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun HeroListItemPreview() {
+    JetHeroesTheme {
+        HeroListItem(
+            name = "Irwan Hermawan",
+            photoUrl = ""
+        )
+    }
+}
